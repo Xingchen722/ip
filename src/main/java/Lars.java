b3a58ce21a0b1108b5b465ade8d5d5bdce19ec47
Xingchen722 <e1459295@u.nus.edu>
2026-01-22 15:22:17 +0800

    Level-1: Added echo feature

--- src/main/java/Lars.java
+++ src/main/java/Lars.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
 public class Lars {
-    public static void main(String[] args){
-        System.out.println("____________________________________________________________");
+    public static void main(String[] args) {
+        System.out.println("------------------------------------------------------------");
         System.out.println("Hello! I'm Lars");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("------------------------------------------------------------");
         Scanner sc = new Scanner(System.in);
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
-                System.out.println("____________________________________________________________");
+                System.out.println("------------------------------------------------------------");
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                System.out.println("------------------------------------------------------------");
                 break;
+            } else {
+                System.out.println(input);
+                System.out.println("------------------------------------------------------------");
             }
         }
     }
