c4c94cd4fb3fe0a852fc1133cce4f8d00a6155d4
Xingchen722 <e1459295@u.nus.edu>
2026-02-03 22:22:18 +0800

    Add JavaDoc comments to classes and methods (A-JavaDoc change)

--- src/test/java/lars/ToDoTest.java
+++ src/test/java/lars/ToDoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ToDoTest {
+
     @Test
     public void dummyTest(){
         assertEquals(2, 2);
