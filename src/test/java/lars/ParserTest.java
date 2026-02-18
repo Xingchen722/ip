90812aecef1079caff8001f61e72a623c5577f0b
Xingchen722 <e1459295@u.nus.edu>
2026-02-18 18:05:10 +0800

    Fix remaining Checkstyle violation in ToDoTest

--- src/test/java/lars/ParserTest.java
+++ src/test/java/lars/ParserTest.java
@@ -1,28 +1,27 @@
 package lars;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 import lars.command.Command;
 import lars.command.ExitCommand;
 import lars.exceptions.LarsException;
 import lars.parser.Parser;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParserTest {
 
     @Test
     public void parser_bye() throws LarsException {
-        // New structure: Parser.parse only accepts string instructions
         Command c = Parser.parse("bye");
 
-        // 检查返回的是否是 ExitCommand，并且 isExit() 是否为 true
         assertTrue(c instanceof ExitCommand, "Bye command should return an instance of ExitCommand.");
         assertTrue(c.isExit(), "ExitCommand should return true for isExit().");
     }
 
     @Test
     public void parser_invalid() {
-        // Check whether the unknown instruction throws an exception
         assertThrows(LarsException.class, () -> {
             Parser.parse("blahblah");
         }, "Undefined commands should throw LarsException.");
@@ -30,7 +29,6 @@ public class ParserTest {
 
     @Test
     public void parser_emptyTodo_exceptionThrown() {
-        // Check whether an empty todo throws an exception
         assertThrows(LarsException.class, () -> {
             Parser.parse("todo");
         }, "Empty todo description should throw LarsException.");
--- src/test/java/lars/StorageTest.java
+++ src/test/java/lars/StorageTest.java
@@ -1,21 +1,24 @@
 package lars;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
 import lars.exceptions.LarsException;
 import lars.storage.Storage;
 import lars.task.Task;
 import lars.task.Todo;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import org.junit.jupiter.api.io.TempDir;
-import java.nio.file.Path;
 
 public class StorageTest {
     @TempDir
     Path tempDir;
 
     @Test
-    public void StorageSaveTest() throws LarsException {
+    public void storageSaveTest() throws LarsException {
         Storage so = new Storage(tempDir.toString());
 
         Task[] tasks = new Task[1];
